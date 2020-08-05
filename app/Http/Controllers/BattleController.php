9c4ae4306c595e1e0bc662d1881bf1fecf680d9a
Elisha Ukpong <Ishukpong418@gmail.com>
2020-08-05 23:52:31 +0100

    added new dep

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -9,6 +9,8 @@ class BattleController extends Controller
 {
     protected $battleAlgorithm = null;
 
+    const USER_ENDPOINT = 'http://microservice_user_nginx/api/v1/user/';
+
     protected function setBattleAlgorithm()
     {
         $this->battleAlgorithm = new Dice();
@@ -19,9 +21,13 @@ class BattleController extends Controller
         $this->setBattleAlgorithm();
 
         $duelResult = $this->battleAlgorithm->fight();
+
+        $client = new Client(['verify' => false]);
+        $player1Data = $client->get( self::USER_ENDPOINT . $request->input('userA'));
+        $player2Data = $client->get( self::USER_ENDPOINT . $request->input('userB'));
         return response()->json( [
-            'player1' => $request->input('userA'),
-            'player2' => $request->input('userB'),
+            'player1' => json_decode($player1Data->getBody()),
+            'player2' => json_decode($player2Data->getBody()),
             'duelResults' => $duelResult
         ] );
     }
